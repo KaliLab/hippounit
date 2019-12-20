fcb0723d347ae216353db3db720a65d1417229f6
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-12-20 16:43:33 +0100

    update conf.py to include sphinx-automodapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,8 @@ release = '1.3.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx-automodapi
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
